5e755567e3a56119a93dfa0f870d74924a0da1b5
jozk <jorgeriffo.u@gmail.com>
2022-08-03 01:58:07 -0400

    Progress: 13.24%

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,17 +1,55 @@
 from django.http import HttpResponse
 import datetime
+from django.template import Template, Context
+from django.template.loader import get_template
+from django.shortcuts  import render
+
+class Persona(object):
+
+    def __init__(self, nombre, apellido):
+
+        self.nombre=nombre
+
+        self.apellido=apellido
+
+
 
 def saludo(request):
 
-    documento="""<html>
-    <body>
-    <h1>
-    Hola alumnos esta es nuestra primera página con Django
-    </h1>
-    </body>
-    </html>"""
+    p1=Persona("Jorgenitales", "Riffo")
 
-    return HttpResponse(documento)
+    ahora=datetime.datetime.now()
+
+    dia=ahora.strftime("%d")
+
+    mes=ahora.strftime("%B")
+
+    agno=ahora.strftime("%Y")
+
+    temas=["Plantillas", "Modelos",
+    "Formularios", "Vistas", "Despliegue"]
+
+    ctx={"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "dia":dia, "mes":mes, "agno":agno, "temas":temas}
+
+    # Lineas obsoletas
+
+        # nombre="Jorge"
+
+        # apellido="Riffo"
+
+        #doc_externo=open("Proyecto1/plantillas/saludo.html")
+
+        #plt=Template(doc_externo.read())
+
+        #doc_externo.close()
+
+        # ctx=Context({"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "dia":dia, "mes":mes, "agno":agno, "temas":temas})
+
+        #doc_externo=get_template('saludo.html')
+        
+        #documento=doc_externo.render(ctx)
+
+    return render(request, "saludo.html", ctx)
 
 def despedida(request):
 
@@ -34,7 +72,6 @@ def dameFecha(request):
 
 def calculaEdad(request, edad, agno):
 
- 
     periodo=agno-2022
     edadFutura=edad+periodo
 
